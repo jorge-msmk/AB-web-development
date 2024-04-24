2da7847554e3d200391adf5b70e90ac4855ad6a4
adriany5357 <adrianaa@msmk.university>
2024-04-24 12:04:18 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainJorgeComponent } from './pages/jorge/main-jorge/main-jorge.component';
 import { HomeComponent } from './shared/home/home.component';
+import { MainAdrianComponent } from './pages/adrian/main-adrian/main-adrian.component';
 
 export const routes: Routes = [
     {
@@ -12,4 +13,8 @@ export const routes: Routes = [
     {
         path: 'jorge', component: MainJorgeComponent
     },
+    {
+        path: 'adrian', component: MainAdrianComponent
+
+    }
 ];
