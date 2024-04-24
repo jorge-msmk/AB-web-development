6770010c6cf4751940ccce95da682b0f0f04e617
yngmarvin <marvinbeatss@gmail.com>
2024-04-24 11:53:51 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainJorgeComponent } from './pages/jorge/main-jorge/main-jorge.component';
 import { HomeComponent } from './shared/home/home.component';
+import { MainRamonComponent } from './pages/ramon/main-ramon/main-ramon.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,8 @@ export const routes: Routes = [
     },
     {
         path: 'jorge', component: MainJorgeComponent
+    },
+    {
+        path: 'ramon', component: MainRamonComponent
     },
 ];
