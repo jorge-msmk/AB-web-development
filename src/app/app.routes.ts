c1a6972a11f3baf3cc2348228477be0360acad56
jorge <jorge.lleonart@msmk.university>
2024-04-24 11:56:56 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainJorgeComponent } from './pages/jorge/main-jorge/main-jorge.component';
 import { HomeComponent } from './shared/home/home.component';
+import { MainOubaidComponent } from './pages/oubaid/main-oubaid/main-oubaid.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,8 @@ export const routes: Routes = [
     },
     {
         path: 'jorge', component: MainJorgeComponent
+    },
+    {
+        path: 'oubaid', component: MainOubaidComponent
     },
 ];
