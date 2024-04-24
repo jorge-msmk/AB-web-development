f11c36b6bc022f254074b6417d6cd8fb4f47bf84
marco <marco@gmail.com>
2024-04-24 10:57:10 +0200

    ab nuevo arqui

---
+++ src/app/pages/marco/components/main-jorge/main-jorge.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-jorge',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-jorge.component.html',
+  styleUrl: './main-jorge.component.css'
+})
+export class MainJorgeComponent {
+
+}
---
+++ src/app/pages/marco/main-marco/main-marco.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-marco',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-marco.component.html',
+  styleUrl: './main-marco.component.css'
+})
+export class MainMarcoComponent {
+
+}
