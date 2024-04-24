6c7e4a37c04a62081e09b452f13ab4937db8b45c
sebas <sebastianop@msmk.university>
2024-04-24 11:55:11 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainJorgeComponent } from './pages/jorge/main-jorge/main-jorge.component';
 import { HomeComponent } from './shared/home/home.component';
+import { MainSebasComponent } from './pages/Sebas/main-sebas/main-sebas.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,8 @@ export const routes: Routes = [
     },
     {
         path: 'jorge', component: MainJorgeComponent
+    },
+    {
+        path: 'Sebas', component: MainSebasComponent
     },
 ];
