5d7cd04a00fff45a6ee981d29d2debcb6ab28aa8
jorge <jorge.lleonart@msmk.university>
2024-04-24 11:10:22 +0200

    arquitectura

---
+++ src/app/pages/oubaid/apuntes/apuntes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-apuntes',
+  standalone: true,
+  imports: [],
+  templateUrl: './apuntes.component.html',
+  styleUrl: './apuntes.component.css'
+})
+export class ApuntesComponent {
+
+}
