21b17e503f367cbe85e1168faced149673d893e2
ibra <ibrahimsm@msmk.university>
2024-04-24 11:53:50 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainJorgeComponent } from './pages/jorge/main-jorge/main-jorge.component';
 import { HomeComponent } from './shared/home/home.component';
+import { MainIbraComponent } from './pages/ibra/main-ibra/main-ibra.component';
 
 export const routes: Routes = [
     {
@@ -12,4 +13,8 @@ export const routes: Routes = [
     {
         path: 'jorge', component: MainJorgeComponent
     },
+
+{
+        path: 'ibra', component: MainIbraComponent
+},
 ];
