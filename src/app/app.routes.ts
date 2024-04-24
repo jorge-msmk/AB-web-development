c5ac2bc80251b57f75c0b4215aa1dea0380d346e
marcosbc@msmk.university <marcosbc@msmk.university>
2024-04-24 11:53:03 +0200

    rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { MainJorgeComponent } from './pages/jorge/main-jorge/main-jorge.component';
 import { HomeComponent } from './shared/home/home.component';
+import { MainMarcosbcComponent } from './pages/MarcosBC/main-marcosbc/main-marcosbc.component';
 
 export const routes: Routes = [
     {
@@ -11,5 +12,8 @@ export const routes: Routes = [
     },
     {
         path: 'jorge', component: MainJorgeComponent
+    },
+    {
+        path: 'marcos', component: MainMarcosbcComponent
     },
 ];
