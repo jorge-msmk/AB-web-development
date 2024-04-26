8e12091967894abce2a68f1759ec6a50c92b8655
sebas <sebastianop@msmk.university>
2024-04-26 10:10:16 +0200

    estilos css

--- src/app/pages/Sebas/main-sebas/main-sebas.component.ts
+++ src/app/pages/Sebas/main-sebas/main-sebas.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { SidemenuComponent } from '../sidemenu/sidemenu.component';
+import { ApuntesComponent } from '../apuntes/apuntes.component';
+import { CommonModule } from '@angular/common';
+
+type OptionsVisibility = {
+  [key: string]: boolean; // Firma de índice para acceso dinámico
+};
 
 @Component({
-  selector: 'app-main-sebas',
+  selector: 'app-main-Sebas',
   standalone: true,
-  imports: [],
-  templateUrl: './main-sebas.component.html',
-  styleUrl: './main-sebas.component.css'
+  imports: [SidemenuComponent, ApuntesComponent, CommonModule],
+  templateUrl: './main-Sebas.component.html',
+  styleUrl: './main-Sebas.component.css'
 })
 export class MainSebasComponent {
+  isOptionVisible: OptionsVisibility = {};
 
+  // Actualiza el estado cuando se recibe el evento del sidebar
+  onOptionChanged(visibleState: OptionsVisibility) {
+    this.isOptionVisible = visibleState;
+    console.log(this.isOptionVisible);
+  }
 }
