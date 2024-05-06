04043c63881d3da608ace2e30089b23b7d3128bf
Jorge Lleonart Tous <jlleonart@knowmadmood.com>
2024-05-06 23:57:45 +0200

    apuntes de terminal

--- src/app/pages/jorge/apuntes/apuntes.component.ts
+++ src/app/pages/jorge/apuntes/apuntes.component.ts
@@ -3,6 +3,8 @@ import { GitComponent } from '../components/git/git.component';
 import { CommonModule } from '@angular/common';
 import { ProgramacionComponent } from '../components/programacion/programacion.component';
 import { AngularComponent } from '../components/angular/angular.component';
+import { HtmlCssJsComponent } from '../components/html-css-js/html-css-js.component';
+import { TerminalComponent } from '../components/terminal/terminal.component';
 
 type OptionsVisibility = {
   [key: string]: boolean; 
@@ -12,12 +14,11 @@ type OptionsVisibility = {
   selector: 'app-apuntes',
   standalone: true,
   schemas: [NO_ERRORS_SCHEMA],
-  imports: [ProgramacionComponent, GitComponent, AngularComponent, CommonModule],
+  imports: [ProgramacionComponent, GitComponent, AngularComponent, CommonModule, ApuntesComponent, HtmlCssJsComponent, TerminalComponent],
   templateUrl: './apuntes.component.html',
   styleUrl: './apuntes.component.css'
 })
 export class ApuntesComponent {
   @Input() optionShow!: OptionsVisibility;
-  
 
 }
