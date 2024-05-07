750bf1bfc2be803c62135a38008fa95b1cdaaad7
Jorge Lleonart Tous <jlleonart@knowmadmood.com>
2024-05-07 16:49:18 +0200

    Terminal notes and navigation done

--- src/app/pages/marcosbc/apuntes/apuntes.component.ts
+++ src/app/pages/marcosbc/apuntes/apuntes.component.ts
@@ -3,6 +3,8 @@ import { GitComponent } from '../components/git/git.component';
 import { CommonModule } from '@angular/common';
 import { ProgramacionComponent } from '../components/programacion/programacion.component';
 import { AngularComponent } from '../components/angular/angular.component';
+import { TerminalComponent } from '../components/terminal/terminal.component';
+import { HtmlCssJsComponent } from '../components/html-css-js/html-css-js.component';
 
 type OptionsVisibility = {
   [key: string]: boolean; 
@@ -12,7 +14,7 @@ type OptionsVisibility = {
   selector: 'app-apuntes',
   standalone: true,
   schemas: [NO_ERRORS_SCHEMA],
-  imports: [ProgramacionComponent, GitComponent, AngularComponent, CommonModule],
+  imports: [ProgramacionComponent, GitComponent, AngularComponent, CommonModule, TerminalComponent, HtmlCssJsComponent],
   templateUrl: './apuntes.component.html',
   styleUrl: './apuntes.component.css'
 })
