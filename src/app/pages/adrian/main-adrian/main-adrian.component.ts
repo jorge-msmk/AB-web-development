132e3af77c1099999e9b2f6cd43120dbc6693e2a
adriany5357 <adrianaa@msmk.university>
2024-05-08 11:55:12 +0200

    css edited

--- src/app/pages/adrian/main-adrian/main-adrian.component.ts
+++ src/app/pages/adrian/main-adrian/main-adrian.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { SidemenuComponent } from '../sidemenu/sidemenu.component';
+import { ApuntesComponent } from '../apuntes/apuntes.component';
+import { CommonModule } from '@angular/common';
+
+type OptionsVisibility = {
+  [key: string]: boolean; // Firma de índice para acceso dinámico
+};
 
 @Component({
   selector: 'app-main-adrian',
   standalone: true,
-  imports: [],
+  imports: [SidemenuComponent, ApuntesComponent, CommonModule],
   templateUrl: './main-adrian.component.html',
   styleUrl: './main-adrian.component.css'
 })
 export class MainAdrianComponent {
+  isOptionVisible: OptionsVisibility = {};
 
+  // Actualiza el estado cuando se recibe el evento del sidebar
+  onOptionChanged(visibleState: OptionsVisibility) {
+    this.isOptionVisible = visibleState;
+    console.log(this.isOptionVisible);
+  }
 }
