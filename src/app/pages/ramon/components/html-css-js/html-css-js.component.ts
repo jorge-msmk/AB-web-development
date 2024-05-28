c40654f5325fb86c8ae0c5b0142181b87cd50b31
yngmarvin <marvinbeatss@gmail.com>
2024-05-28 20:08:42 +0200

    estilo-css

--- src/app/pages/ramon/components/html-css-js/html-css-js.component.ts
+++ src/app/pages/ramon/components/html-css-js/html-css-js.component.ts
@@ -1,1 +1,13 @@
 
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-html-css-js',
+  standalone: true,
+  imports: [],
+  templateUrl: './html-css-js.component.html',
+  styleUrl: './styles.css'
+})
+export class HtmlCssJsComponent {
+
+}
